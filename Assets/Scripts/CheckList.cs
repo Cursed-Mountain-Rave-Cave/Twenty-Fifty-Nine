cccac7e4000aac8177875216e119ee0fc669dc60
Gleb Alekseev <azzar.bam@gmail.com>
2020-07-18 11:07:12 +0200

    CheckList.cs => converted to Generic

--- Assets/Scripts/CheckList.cs
+++ Assets/Scripts/CheckList.cs
@@ -2,25 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public static class CheckList 
+public static class CheckList<T> 
 {
-    private static IEnumerable<string> list;
-    public static bool Init(IEnumerable<string> list)
+    private static IEnumerable<T> list;
+    public static bool Init(IEnumerable<T> list)
     {
-        CheckList.list = list;
-        return CheckList.list == null? false : true;
+        CheckList<T>.list = list;
+        return CheckList<T>.list == null? false : true;
     }
-    public static bool Check(string item)
+    public static bool Check(T item)
     {
-        return CheckList.list == null?CheckList.list.Contains(item) : false;
+        return CheckList<T>.list == null?CheckList<T>.list.Contains(item) : false;
     }
-    public static List<string> getList()
+    public static List<T> getList()
     {
-        return  CheckList.list == null? CheckList.list.ToList() : null;
+        return  CheckList<T>.list == null? CheckList<T>.list.ToList() : null;
     }
-    public static IEnumerable<string> GetIEnumerableList()
+    public static IEnumerable<T> GetIEnumerableList()
     {
-        return CheckList.list;
+        return CheckList<T>.list;
     }
 
 }
