dca3d0b412aa640bbc7a094c456725ab6cd11928
Gleb Alekseev <azzar.bam@gmail.com>
2020-07-18 15:22:21 +0200

    CheckList fix added file:   Assets/Scripts/CheckListView.cs added file:   Assets/Scripts/CheckListView.cs.meta

---
+++ Assets/Scripts/CheckListView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckListView : MonoBehaviour
+{
+    public List<GameObject> List = new List<GameObject>();
+    public Dictionary<string,GameObject> Dict = new Dictionary<string,GameObject>();
+    void Start()
+    {
+        CheckList<string>.Init(new List<string>(){
+            "baltika","pivo","pivas","nevskoe","jenia"
+        });
+        CheckList<string>.getList()?.ForEach((x)=>{
+            if(List.GetEnumerator().MoveNext())
+                this.Dict.Add(x,List.GetEnumerator().Current);
+        });
+    }
+    void Update()
+    {
+        
+    }
+}
+
