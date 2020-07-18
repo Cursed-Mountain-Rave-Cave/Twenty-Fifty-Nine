a501cd6852d4733371000fb5a97f5a8f22238d24
Gleb Alekseev <azzar.bam@gmail.com>
2020-07-18 10:53:23 +0200

    new file:   Assets/Scripts/CheckList.cs

---
+++ Assets/Scripts/CheckList.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public static class CheckList 
+{
+    private static IEnumerable<string> list;
+    public static bool Init(IEnumerable<string> list)
+    {
+        CheckList.list = list;
+        return CheckList.list == null? false : true;
+    }
+    public static bool Check(string item)
+    {
+        return CheckList.list == null?CheckList.list.Contains(item) : false;
+    }
+    public static List<string> getList()
+    {
+        return  CheckList.list == null? CheckList.list.ToList() : null;
+    }
+    public static IEnumerable<string> GetIEnumerableList()
+    {
+        return CheckList.list;
+    }
+
+}
