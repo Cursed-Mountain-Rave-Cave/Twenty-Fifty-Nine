c395ced6325586a69dfbd8880837af7a1ce634ca
Gleb Alekseev <azzar.bam@gmail.com>
2020-07-19 03:00:20 +0200

    add Showing of TimePhone

--- Assets/Scripts/CheckListShower.cs
+++ Assets/Scripts/CheckListShower.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class CheckListShower : MonoBehaviour
 {
     public GameObject ViewList;
+    public GameObject Phone;
+    public bool onShow;
     void Start()
     {
         ViewList?.SetActive(false);
+        Phone?.SetActive(false);
+        onShow = false;
     }
     void Update()
     {
         if(Input.GetKeyUp(KeyCode.E))
         {
             ViewList?.SetActive(false);
+            onShow = false;
         }
-        if(Input.GetKeyDown(KeyCode.E))
+        if(Input.GetKeyDown(KeyCode.E) && !onShow)
         {
             ViewList?.SetActive(true);
+            onShow = true;
+        }
+        if(Input.GetKeyUp(KeyCode.Q))
+        {
+            Phone?.SetActive(false);
+            onShow = false;
+        }
+        if(Input.GetKeyDown(KeyCode.Q) && !onShow)
+        {
+            Phone?.SetActive(true);
+             onShow = true;
         }
     }
 }
