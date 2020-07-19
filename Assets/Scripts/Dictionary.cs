1ae05d92610dd23e1f4f90540334c24e6354096d
Glebanya <azzar.bam@gmail.com>
2020-07-19 17:37:36 +0200

    added description

--- Assets/Scripts/Dictionary.cs
+++ Assets/Scripts/Dictionary.cs
@@ -6,11 +6,12 @@ public static class TagDictionary
 {
     static System.Collections.Generic.Dictionary<string,string> Obj = new System.Collections.Generic.Dictionary<string, string>()
     {
-        {"pivo_green",""},
-        {"pivo_brown",""},
-        {"vodka",""},
-        {"shampun",""},
-        {"sidr",""}
+        {"pivo_green","Пиво ЗЕЛЕНОЕ"},
+        {"sidr","СИДР"},
+        {"pivo_brown","ПИВО КОРИЧНЕВОЕ"},
+        {"shampun","ШамПанское 'Шампунь'"},
+        {"vodka","Водка Ядреная"},
+        
     };
     public static bool GetValue(string str,out string value)
     {
