35089c3a4ad4b7004542c0c7a5e30cebde7561b5
Gleb Alekseev <azzar.bam@gmail.com>
2020-07-19 13:11:29 +0200

    added Dictionary tag to String Dictionary

---
+++ Assets/Scripts/Dictionary.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TagDictionary
+{
+    static System.Collections.Generic.Dictionary<string,string> Obj = new System.Collections.Generic.Dictionary<string, string>()
+    {
+        {"baltica","Балтика 9"}
+    };
+    public static bool GetValue(string str,out string value)
+    {
+        return Obj.TryGetValue(str,out value);
+    }
+}
