5c53bfb4a47506419fb826e07096eaf1a7055a2c
EZavdovev <45925246+EZavdovev@users.noreply.github.com>
2020-07-20 01:23:48 +0200

    fixik

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,15 +9,16 @@ public class PlayerController : MonoBehaviour
     public Camera not_camera;
     public CharacterController charController;
     public float gravity;
+    public AudioSource walkSound;
     void Start()
     {
-       Cursor.visible = false; 
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Cursor.visible = false;
 
         if (Math.Abs(not_camera.transform.rotation.eulerAngles.x - Time.deltaTime * cameraSpeed * Input.GetAxis("Mouse Y") - 180) > 90)
         {
@@ -27,6 +28,16 @@ public class PlayerController : MonoBehaviour
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+
+        if((x + z) != 0 && !walkSound.isPlaying)
+        {
+            walkSound.Play();
+        }
+        else if ((x + z) == 0)
+        {
+            walkSound.Stop();
+        }
+
         float speed = normalSpeed;
 
         if (Input.GetKey(KeyCode.LeftShift))
@@ -35,6 +46,7 @@ public class PlayerController : MonoBehaviour
         }
 
         Vector3 move = transform.right * x + transform.forward * z;
+
         charController.Move(move * speed * Time.deltaTime);
 
         charController.Move(new Vector3(0,gravity,0) * Time.deltaTime);
