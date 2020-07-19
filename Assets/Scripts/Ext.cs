b9c7adab1bf5d954782231230d1d53d23180280c
Glebanya <azzar.bam@gmail.com>
2020-07-19 17:09:04 +0200

    added Ext for Collections TagList

---
+++ Assets/Scripts/Ext.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+public static class Ext
+{
+    public static T getRandom<T>(this IEnumerable<T> source)
+    {
+        return source.OrderBy(x => Guid.NewGuid()).FirstOrDefault();
+    }
+}
---
+++ Assets/Scripts/TagList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TagList
+{
+    static List<string> list = new List<string>(){
+        "pivo_green",
+        "pivo_brown",
+        "vodka",
+        "shampun",
+        "sidr",
+    };
+}
